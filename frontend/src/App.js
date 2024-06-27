ff7bc452ae60658d045d720dd3be0d8260dd8ba7
Nomekuma <t.guness35@gmail.com>
2024-06-27 10:45:14 +0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+function App() {
+  const [pokemon, setPokemon] = useState({});
+  const [pokemonName, setPokemonName] = useState("pikachu");
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/${pokemonName}`);
+        const data = await response.json();
+        setPokemon(data[0]);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchPokemon();
+  }, [pokemonName]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPokemonName(e.target.elements[0].value);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <button type="submit">Search</button>
+      </form>
+      {pokemon.name && <h1>{pokemon.name}</h1>}
+      {pokemon.weight && <h1>{pokemon.weight}</h1>}
+      {pokemon.sprites && <img src={pokemon.sprites} alt={pokemon.name} />}
+    </div>
+  );
+}
+
+export default App;
